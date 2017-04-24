9acb3c98810ce1d2cfd407dee039c17a1318149c
sussette <succette_21@hotmail.com>
2017-04-24 18:26:35 -0500

    Installation of packages

---
+++ HeatmapCantonsCostaRica.R
@@ -0,0 +1,17 @@
+#Install the libraries of spatial utilities
+install.packages(c("sp", "spdep", "classInt", "maptools", "RgoogleMaps", "rgdal", "ggplot2",
+                   "weights", "car", "rgl", "GeoXp","Matrix"))
+
+#Open libraries of spatial utilities
+library(sp)
+library(Matrix)
+library(spdep)
+library(classInt)
+library(maptools)
+library(RgoogleMaps)
+library(rgdal)
+library(ggplot2)
+library(weights)
+library(car)
+library(rgl)
+library(GeoXp)
